00c866ad253832ae1e47d372ec2ac422258c7047
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-09-07 14:23:46 -0300

    Aprendendo condições

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 62
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida!')
+} 
+console.log(`Dirija com cuidado!`)
