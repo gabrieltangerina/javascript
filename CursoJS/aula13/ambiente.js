192c7e1fe71d582e9ef060338c0aa0df2f089128
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-09-10 10:13:27 -0300

    Aprendendo o uso de repetições

---
+++ CursoJS/aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+/*while(c <= 10){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+do{
+    console.log(`Etapa ${c}`)
+    c++
+} while(c <=10) 
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
