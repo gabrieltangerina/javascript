cc24756e08964620a807bb035320d3a618a50d8e
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-09-11 21:00:24 -0300

    Aprendendo Array, vetor ou variáveis compostas

---
+++ CursoJS/aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5,7,4,9,6]
+
+num.sort()
+console.log(`O vetor tem ${num.length} posições/elementos.`)
+console.log(`O primeiro valor é ${num[0]}.`)
+console.log(num)
---
+++ CursoJS/aula15/vetornatela.js
@@ -0,0 +1,17 @@
+let valores = [3,5,9,3,2,9,1,4,8]
+
+console.log(`O valor ${valores[indexOf(2)]} está na posição ${valores.indexOf(2)}`)
+
+
+
+/*
+var posição = 0
+while(posição<valores.length){
+    console.log(`A posição ${posição} tem o valor ${valores[posição]}`)
+    posição++
+}
+
+for(let posição=0; posição < valores.length; posição++ ){
+    console.log(`A posição ${posição} tem o valor ${valores[posição]}`)
+}
+*/
