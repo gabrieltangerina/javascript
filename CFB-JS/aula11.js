82c2eb48f5c30a44f5bd8ab0983f178eec4b79d8
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-11-08 17:11:57 -0300

    Aprendendo JavaScript

---
+++ CFB-JS/aula11.js
@@ -0,0 +1,10 @@
+let v1=10
+let v2="10"
+let v3 = v1===v2
+let v4= {nome:"Gabriel"}
+
+console.log("O valor " + v1 + " é do tipo:" + typeof(v1))
+console.log("O valor " + v2 + " é do tipo:" + typeof(v2)) 
+console.log("O valor " + v3 + " é do tipo:" + typeof(v3))
+console.log("O valor " + v4 + " é do tipo:" + typeof(v4))
+console.log("O valor " + v5 + " é do tipo:" + typeof(v5))
