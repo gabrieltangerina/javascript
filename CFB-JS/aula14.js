689cd4d4e3fc1a17f11a1754e879638e50ccb42e
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-11-08 21:23:15 -0300

    Create aula14.js

---
+++ CFB-JS/aula14.js
@@ -0,0 +1,16 @@
+let colocacao= 1
+
+switch(colocacao){
+    case 1: 
+        console.log("Primeiro lugar")
+        break
+    case 2:
+        console.log("Segundo lugar")
+        break
+    case 3:
+        console.log("Terceiro lugar")
+        break
+    default:
+        console.log("Não foi premiado")
+        break
+}
