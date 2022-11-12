31ef5a3797164038812dd5d7e303e9171ba82cd4
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-11-12 19:56:32 -0300

    Break e Continue

---
+++ CFB-JS/aula17.js
@@ -0,0 +1,18 @@
+let n=5
+let fat=1
+
+while(n>=1){ 
+    fat = fat * n 
+    n--
+}
+
+/*
+Começa no 
+1.5
+5.4
+20.3
+60.2
+120.1
+*/ 
+
+console.log(fat)
---
+++ CFB-JS/aula19.js
@@ -0,0 +1,12 @@
+let n=0
+let num=50
+let pares = 0
+
+while(n<num){
+    if(n%2==0){
+        pares++    
+    }
+}
+
+console.log(pares)
+console.log('Fim do programa')
