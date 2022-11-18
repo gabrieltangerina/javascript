e9fcd9aa465fc77665dcd6ebd5d27941d3838c63
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-11-17 22:34:46 -0300

    Aprendendo sobre funções MAP

---
+++ CFB-JS/aula28.js
@@ -0,0 +1,12 @@
+let el=document.getElementsByTagName("div")
+el=[...el]
+el.map((e,i)=>{
+    console.log(e.innerHTML)
+})
+
+// const cursos=['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
+// let c=cursos.map((el,i)=>{
+//     return el
+//})
+
+// console.log(c)
