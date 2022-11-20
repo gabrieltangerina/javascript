b46d216a345051b5bd8df2bceb49358bc63c6a50
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-11-19 21:33:17 -0300

    Update teste.js

--- CFB-JS/teste.js
+++ CFB-JS/teste.js
@@ -1,6 +1,26 @@
-// function getRandomNumber(){
-//     return Math.random
+// let auto = ['moto','carro','caminhonete','onibus']
+
+// for(i in auto){
+//     console.log(i)
 // }
 
-const getRandomNumber = () => Math.random
+let num = [5,6,3,2,1]
+// num[5] = 8
+// num.push(7)
+
+// console.log(`Nosso vetor é o: ${num}`)
+
+// console.log(`O tamanho do meu array é: ${num.length}`)
+
+// console.log(`Colocando em ordem: ${num.sort()}`)
+
+let i=0
+while(i<num.length){
+    console.log(num[i])
+    i++
+}
+
+// for(i of num){
+//     console.log(i)
+// }
 
