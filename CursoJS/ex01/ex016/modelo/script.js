d4fc5cefd572607f03f6f7d56ce615401b79353a
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-11-25 19:43:45 -0300

    Update script.js

--- CursoJS/ex01/ex016/modelo/script.js
+++ CursoJS/ex01/ex016/modelo/script.js
@@ -1,27 +1,55 @@
 function contar(){
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
+    let pas = document.getElementById('txtp')
     let res = document.getElementById('res')
 
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        res.innerHTML = 'Impossível contar!'
-    } else{
-        let i = Number(ini.value) //pegando o valor de 'ini'e convertendo em número
+    if(ini.value == 0 || fim.value == 0 || pas.value == 0){
+        res.innerHTML = 'Preecha corretamente os espaços acima!'
+    }else{
+        let i = Number(ini.value)
         let f = Number(fim.value)
-        let p = Number(passo.value)
+        let p = Number(pas.value)
 
         if(i < f){
-            //Contagem crescente
-            for(let c = i; c <= f; c = c + p){
-                res.innerHTML += `${c} -->` 
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} -->` // o += serve para acrescentar o próximo número
             }
-        } else{
-            //Contagem regressiva 
+        }else{
+            // for(let c = i; c >= f; c -= p){
+            //     res.innerHTML += `${c} -->`
             for(let c = i; c >= f; c -= p){
-                res.innerHTML += `${c} -->`
+                res.innerHTML += `${c}-->`
             }
         }
         res.innerHTML += 'FIM!'
     }
 }
+
+// function contar(){
+//     let ini = document.getElementById('txti')
+//     let fim = document.getElementById('txtf')
+//     let passo = document.getElementById('txtp')
+//     let res = document.getElementById('res')
+
+//     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+//         res.innerHTML = 'Impossível contar!'
+//     } else{
+//         let i = Number(ini.value) //pegando o valor de 'ini'e convertendo em número
+//         let f = Number(fim.value)
+//         let p = Number(passo.value)
+
+//         if(i < f){
+//             //Contagem crescente
+//             for(let c = i; c <= f; c = c + p){
+//                 res.innerHTML += `${c} -->` 
+//             }
+//         } else{
+//             //Contagem regressiva 
+//             for(let c = i; c >= f; c -= p){
+//                 res.innerHTML += `${c} -->`
+//             }
+//         }
+//         res.innerHTML += 'FIM!'
+//     }
+// }
