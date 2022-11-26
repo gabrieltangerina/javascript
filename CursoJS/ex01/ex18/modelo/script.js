169bb3f07f2f4a62040668c83c39a1ae7b7146db
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-11-26 18:10:22 -0300

    Aprendendo JavaScript

---
+++ CursoJS/ex01/ex18/modelo/script.js
@@ -0,0 +1,32 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l){
+
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value)){
+        
+    }else{
+        window.alert('Valor inválido ou ja encontrado na lista.')
+    }
+}
