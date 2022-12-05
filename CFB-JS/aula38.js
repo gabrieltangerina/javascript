39cf1f57823b57c29ff789b627c26a5d87396f57
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-12-05 14:09:40 -0300

    Update aula38.js

--- CFB-JS/aula38.js
+++ CFB-JS/aula38.js
@@ -1,9 +1,19 @@
 const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".curso")]
 
+// console.log(caixa1.hasChildNodes())
+// console.log(btn_c[0].hasChildNodes()) //A resposta será: TRUE -- O filho é o texto(text)
+// console.log(btn_c[0].childNodes) // A resposta será: Nodelist [text]
+// console.log(btn_c[0].children)
+
+// console.log(caixa1.children.length > 0 ? "Possui filhos" : "Não possui filhos")
+
+// console.log(caixa1.firstElementChild.innerHTML="TESTE")
+// console.log(caixa1.children[1].innerHTML="Segundo!")
+
 // console.log(caixa1.firstElementChild)
 // console.log(caixa1.lastElementChild)
-console.log(caixa1.children)
+// console.log(caixa1.children)
 
 // console.log(btn_c[0].getRootNode())
 // console.log(btn_c[0].ownerDocument)
