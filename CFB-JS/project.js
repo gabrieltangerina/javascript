c7074f93e2f22de09c4cb40ce0f3483c7bf7ab9e
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-12-10 18:04:21 -0300

    Working...

---
+++ CFB-JS/project.js
@@ -0,0 +1,11 @@
+boxAddElement=document.querySelector("boxAdd")
+btnAddElement=document.getElementById("#btnAddElement")
+
+btnAddElement.addEventListener("click", ()=>{
+    console.log("deu certo")
+})
+
+boxAddElement.addEventListener("click", ()=>{
+    console.log("deu certo aqui")
+})
+
