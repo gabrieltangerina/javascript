a58aae9cb9d506915b5c2aa563e16aa63ba98fb6
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-12-11 18:35:03 -0300

    Aprendendo sobre funções

---
+++ CursoUdemy/função/index.js
@@ -0,0 +1,25 @@
+function soma(x, y){
+    const resultado = x + y
+    return resultado
+}
+
+console.log(soma(2, 2))
+
+
+// //Função anônima
+const raizFuncaoAnonima = function (n){
+    return n ** 0.5
+};
+
+console.log(raiz(9))
+
+// //Arrow function
+
+const raizArrowFunction = (n)=>{
+
+}
+
+//Arrow function reduzida
+
+const raizArrowFunctionReduzida = n => n ** 0.5
+console.log(raizArrowFunctionReduzida(25)) 
