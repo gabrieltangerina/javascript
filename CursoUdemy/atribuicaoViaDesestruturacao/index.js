f4fa3285c7a844d20740bd936ebaae9e720de67e
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-12-15 15:00:27 -0300

    Aprendendo atribuição via desestruturação

---
+++ CursoUdemy/atribuicaoViaDesestruturacao/index.js
@@ -0,0 +1,7 @@
+const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+const [n1, n2, ...resto] = numeros;
+console.log(n1,n2);
+console.log(resto);
+
+const letras = [ ['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'] ];
+console.log(letras[1][2])
--- CursoUdemy/exercicioData/modelo/assets/js/main.js
+++ CursoUdemy/exercicioData/modelo/assets/js/main.js
@@ -1,51 +1,53 @@
-const text = document.querySelector("#text");
-const hours = document.querySelector("#hours")
+// const text = document.querySelector("#text");
+// const hours = document.querySelector("#hours")
 
+// const data = new Date();
+
+// const dateOfWeek = data.getDay();
+// const dateDay = data.getDate();
+// const dateOfMonth = data.getMonth();
+// const dateYear = data.getFullYear();
+
+// let textHours = leftZero(data.getHours());
+// let textMinutes = leftZero(data.getMinutes());
+// let textSecondes = leftZero(data.getSeconds());
+
+// function leftZero(num){
+//     return num >= 10 ? num : `0${num}`;
+// }
+
+// function nameDayOfWeek(){
+//     const daysOfWeek = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+    
+//     return daysOfWeek[dateOfWeek];
+// }
+
+// function nameMonth(){
+//     const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+//     return months[dateOfMonth];
+// }
+
+
+
+// hours.innerHTML = `${textHours}:${textMinutes}:${textSecondes}`
+// text.innerHTML = `${nameDayOfWeek()}, ${dateDay} de ${nameMonth()} de ${dateYear} `;
+
+const hours = document.querySelector("#hours");
+const text = document.querySelector("#text");
 const data = new Date();
 
-const dateOfWeek = data.getDay();
-const dateDay = data.getDate();
-const dateOfMonth = data.getMonth();
-const dateYear = data.getFullYear();
+let textHours = leftZero(data.getHours());
+let textMinutes = leftZero(data.getMinutes());
+let textSecondes = leftZero(data.getSeconds());
 
-const textHours = data.getHours();
-const textMinutes = data.getMinutes();
-const textSecondes = data.getSeconds();
-
-
-function nameDayOfWeek(){
-    let textDateWeek
-    if (dateOfWeek === 0) textDateWeek = "Domingo";
-    if (dateOfWeek === 1) textDateWeek = "Segunda-feira";
-    if (dateOfWeek === 2) textDateWeek = "Terça-feira";
-    if (dateOfWeek === 3) textDateWeek = "Quarta-feira";
-    if (dateOfWeek === 4) textDateWeek = "Quinta-feira";
-    if (dateOfWeek === 5) textDateWeek = "Sexta-feira";
-    if (dateOfWeek === 6) textDateWeek = "Sábado";
-    return textDateWeek
-}
-
-function nameMonth(){
-    let textDateMonth
-    if(dateOfMonth === 0) textDateMonth = "Janeiro";
-    if(dateOfMonth === 1) textDateMonth = "Fevereiro";
-    if(dateOfMonth === 2) textDateMonth = "Março";
-    if(dateOfMonth === 3) textDateMonth = "Abril";
-    if(dateOfMonth === 4) textDateMonth = "Maio";
-    if(dateOfMonth === 5) textDateMonth = "Junho";
-    if(dateOfMonth === 6) textDateMonth = "Julho";
-    if(dateOfMonth === 7) textDateMonth = "Agosto";
-    if(dateOfMonth === 8) textDateMonth = "Setembro";
-    if(dateOfMonth === 9) textDateMonth = "Outubro";
-    if(dateOfMonth === 10) textDateMonth = "Novembro";
-    if(dateOfMonth === 11) textDateMonth = "Dezembro";
-    
-    return textDateMonth
+function leftZero(num){
+    return num >= 10 ? num : `0${num}`;
 }
 
 
 hours.innerHTML = `${textHours}:${textMinutes}:${textSecondes}`
-text.innerHTML = `${nameDayOfWeek()}, ${dateDay} de ${nameMonth()} de ${dateYear} `
+text.innerHTML = data.toLocaleDateString("pt-BR", {dateStyle: 'full',})
 
-
-
+//O do video estava assim:
+// h1.innerHTML = data.toLocaleDateString('pt-br', {dateStyle: 'full', timeStyle: 'short' });
