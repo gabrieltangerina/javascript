f9e068a2334283810a4e6a620c2f0e29ee99de41
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-12-17 23:28:53 -0300

    Colocando em prática FOR OF

---
+++ CursoUdemy/exercicioNodeList/assets/js/main.js
@@ -0,0 +1,11 @@
+const containerParagrafos = document.querySelector(".paragrafos");
+const paragrafos = document.querySelectorAll("p");
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+// console.log(backgroundColorBody);
+
+for (let p of paragrafos){
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = "#fff";
+}
