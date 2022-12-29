732714286c06e6fa37d3524dc13d683035dbeaaf
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-12-29 20:34:34 -0300

    Estudando o projeto calculadora

--- CursoUdemy/estudandoProjetoCalculadora/assets/js/script.js
+++ CursoUdemy/estudandoProjetoCalculadora/assets/js/script.js
@@ -1,25 +1,24 @@
 // Falar sobre o método slice no caderno;
 
-function criaCalculadora(){
-    
+function criaFuncao(){
 
     return{
-        display: document.querySelector(".display"),
+        display: document.querySelector('.display'),
 
         inicia(){
             this.cliqueBotao();
         },
 
-        btnParaDisplay(valor){
+        valorBotao(valor){
             this.display.value += valor;
         },
 
-        clearDisplay(){
-            this.display.value = '';
+        clearInput(){
+            this.display.value = ' ';
         },
 
-        apagarUm(){
-            this.display.value = this.display.value.slice(0, -1)
+        apagaUm(){
+            this.display.value = this.display.value.slice(0, -1);
         },
 
         realizaConta(){
@@ -29,42 +28,42 @@ function criaCalculadora(){
                 conta = eval(conta);
 
                 if(!conta){
-                    alert("Conta INVÁLIDA");
+                    alert("Conta INVÁLIDA!");
                     return
                 }
 
                 this.display.value = String(conta);
-            } catch(e){
+            }catch(e){
                 alert("Conta INVÁLIDA");
                 return
             }
+
         },
 
         cliqueBotao(){
-            document.addEventListener("click", e =>{
+            document.addEventListener('click', e => {
                 el = e.target;
 
-                if(el.classList.contains("btn-num")){
-                    this.btnParaDisplay(el.innerText);
+                if(el.classList.contains('btn-num')){
+                    this.valorBotao(el.innerText);
                 }
 
-                if(el.classList.contains("btn-clear")){
-                    this.clearDisplay();
+                if(el.classList.contains('btn-clear')){
+                    this.clearInput();
                 }
 
-                if(el.classList.contains("btn-del")){
-                    this.apagarUm();
-                }  
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm();
+                }
 
-                if(el.classList.contains("btn-eq")){
+                if(el.classList.contains('btn-eq')){
                     this.realizaConta();
                 }
             })
         },
-
     }
 
-}
+};
 
-const calculadora = criaCalculadora();
+const calculadora = criaFuncao();
 calculadora.inicia();
