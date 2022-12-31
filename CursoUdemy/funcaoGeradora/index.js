d3e0b77ff992976bff9d92a64f5ccf51bc1218bd
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-12-31 20:25:20 -0300

    Aprendendo sobre Funções Geradoras

--- CursoUdemy/funcaoGeradora/index.js
+++ CursoUdemy/funcaoGeradora/index.js
@@ -1,11 +1,11 @@
-function* geradora1(){
-    // Código...
-    yield 'Valor 1';
-    // Código...
-    yield 'Valor 2';
-    // Código
-    yield 'Valor 3';
-}
+// function* geradora1(){
+//     // Código...
+//     yield 'Valor 1';
+//     // Código...
+//     yield 'Valor 2';
+//     // Código
+//     yield 'Valor 3';
+// }
 
 // const g1 = geradora1();
 // console.log(g1.next().value);
@@ -17,17 +17,35 @@ function* geradora1(){
     // console.log(valor);
 // }
 
-function* geradora2(){
-    let i = 0;
-    while(true){
-        yield i;
-        i++;
-    }
+// function* geradora2(){
+//     let i = 0;
+//     while(true){
+//         yield i;
+//         i++;
+//     }
+// }
+
+// const g2 = geradora2();
+
+// console.log(g2.next().value);
+// console.log(g2.next().value);
+// console.log(g2.next().value);
+// console.log(g2.next().value);
+
+function* geradora3 () {
+    yield 1;
+    yield 2;
+    yield 3;
 }
 
-const g2 = geradora2();
+function* geradora4() {
+    yield* geradora3();
+    yield 4;
+    yield 5;
+    yield 6;
+}
 
-console.log(g2.next().value);
-console.log(g2.next().value);
-console.log(g2.next().value);
-console.log(g2.next().value);
+const g4 = geradora4();
+for(let valor of g4){
+    console.log(valor);
+}
