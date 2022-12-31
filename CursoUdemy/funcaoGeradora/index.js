eb48849cac3b3e0a082836dca920df1354325c42
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-12-31 20:11:03 -0300

    Aprendendo sobre Função Geradora

---
+++ CursoUdemy/funcaoGeradora/index.js
@@ -0,0 +1,33 @@
+function* geradora1(){
+    // Código...
+    yield 'Valor 1';
+    // Código...
+    yield 'Valor 2';
+    // Código
+    yield 'Valor 3';
+}
+
+// const g1 = geradora1();
+// console.log(g1.next().value);
+// console.log(g1.next().done);
+// console.log(g1.next());
+// console.log(g1.next());
+
+// for(let valor of g1){
+    // console.log(valor);
+// }
+
+function* geradora2(){
+    let i = 0;
+    while(true){
+        yield i;
+        i++;
+    }
+}
+
+const g2 = geradora2();
+
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
