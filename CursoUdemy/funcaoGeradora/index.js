a7bae9a79324e5a025e391009e9fa51b609b7f61
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-01-02 00:18:17 -0300

    Aprendendo Função Geradora

--- CursoUdemy/funcaoGeradora/index.js
+++ CursoUdemy/funcaoGeradora/index.js
@@ -1,36 +1,36 @@
-// function* geradora1(){
-//     // Código...
-//     yield 'Valor 1';
-//     // Código...
-//     yield 'Valor 2';
-//     // Código
-//     yield 'Valor 3';
-// }
+function* geradora1(){
+    // Código...
+    yield 'Valor 1';
+    // Código...
+    yield 'Valor 2';
+    // Código
+    yield 'Valor 3';
+}
 
-// const g1 = geradora1();
-// console.log(g1.next().value);
-// console.log(g1.next().done);
-// console.log(g1.next());
-// console.log(g1.next());
+const g1 = geradora1();
+console.log(g1.next().value);
+console.log(g1.next().done);
+console.log(g1.next());
+console.log(g1.next());
 
-// for(let valor of g1){
-    // console.log(valor);
-// }
+for(let valor of g1){
+    console.log(valor);
+}
 
-// function* geradora2(){
-//     let i = 0;
-//     while(true){
-//         yield i;
-//         i++;
-//     }
-// }
+function* geradora2(){
+    let i = 0;
+    while(true){
+        yield i;
+        i++;
+    }
+}
 
-// const g2 = geradora2();
+const g2 = geradora2();
 
-// console.log(g2.next().value);
-// console.log(g2.next().value);
-// console.log(g2.next().value);
-// console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
 
 function* geradora3 () {
     yield 1;
@@ -49,3 +49,28 @@ const g4 = geradora4();
 for(let valor of g4){
     console.log(valor);
 }
+
+function* geradora5(){
+    yield () =>{
+        console.log('Vim do YIELD 1');
+    }
+
+    return function(){
+        console.log('Fim do programa!');
+    }
+
+    yield function(){
+        console.log("Vim do YIELD 2");
+    }
+}
+
+const g5 = geradora5();
+
+const func1 = g5.next().value;
+const func2 = g5.next().value;
+const func3 = g5.next().value;
+
+func1();
+func2();
+func3();
+
