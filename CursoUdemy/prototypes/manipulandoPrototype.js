4ed42926c753f2d42e22b8879b1a719ec2aef4b0
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-01-09 22:09:33 -0300

    Aprendendo mais sobre Prototype

--- CursoUdemy/prototypes/manipulandoPrototype.js
+++ CursoUdemy/prototypes/manipulandoPrototype.js
@@ -35,7 +35,42 @@ Produto.prototype.aumento = function (aumentoDe) {
     this.preco = this.preco + (this.preco * (aumentoDe / 100));
 };
 
+Produto.prototype.esgotado = function (){
+    this.nome = 'ESGOTADO';
+}
+
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+}
+Object.setPrototypeOf(p2, Produto.prototype);
+
 const p1 = new Produto('Camisa', 100);
 p1.desconto(50);
-console.log(p1);
+// console.log(p1);
 
+p2.aumento(10);
+// p2.esgotado();
+// console.log(p2);
+
+
+const p3 = Object.create(Produto.prototype, {
+    nome: {
+        writable: true,
+        value: 'Chaveiro'
+    },
+    preco: {
+        // configurable: true,
+        // enumerable: true,
+        writable: true,
+        value: 200
+    }
+});
+
+// p3.esgotado();
+p3.desconto(50);
+console.log(p3);
+
+// Anotar sobre a necessidade do 'Writable' 
+// Falar também que ele pose receber os outros valores dos métodos do objeto
+// Falar que se estivesse Object.prototye o objeto estaria vazio 
