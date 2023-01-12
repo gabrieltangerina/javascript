cea491293fc78c9c6419e1fed15e478a3d568a0b
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-01-11 22:07:22 -0300

    Aprendendo sobre polimorfismo

--- CursoUdemy/polimorfismo/index.js
+++ CursoUdemy/polimorfismo/index.js
@@ -7,11 +7,12 @@ function Conta(agencia, conta, saldo){
 
 Conta.prototype.sacar = function(valor){
     if(this.saldo < valor) {
-        this.verSaldo();
+        console.log(`Saldo insuficiente: R$${this.saldo.toFixed(2)}`);
         return;
     }
 
     this.saldo -= valor;
+    this.verSaldo();
 }
 
 Conta.prototype.depositar = function(valor){
@@ -23,5 +24,24 @@ Conta.prototype.verSaldo = function(){
     console.log(`Agência: ${this.agencia}/${this.conta} | Saldo: R$${this.saldo.toFixed(2)}`);
 };
 
-const conta1 = new Conta('Bradesco', 1732, 100);
-console.log(conta1);
+function ContaCorrente(agencia, conta, saldo, limite){
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function(valor){
+    if(valor > (this.saldo + this.limite)){
+        console.log(`Saldo insuficiente: ${this.saldo.toFixed(2)}`);
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+const cc = new ContaCorrente('Caixa', 1783, 0, 100);
+cc.sacar(100);
+cc.sacar(2);
