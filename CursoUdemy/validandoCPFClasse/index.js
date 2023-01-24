14cad1c64749a926ea6d2c2e1b39265abbb12a17
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-01-24 10:50:30 -0300

    Estudando sobre o cpf em classe

--- CursoUdemy/validandoCPFClasse/index.js
+++ CursoUdemy/validandoCPFClasse/index.js
@@ -1,53 +1,15 @@
-// Código para ver se é uma sequência:
-// return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length) === this.cpfLimpo;
-// ou
-// return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
-// ou
-// return this.cpfLimpo[0].repeat(11) === this.cpfLimpo;
-
-// 070.987.720-03
-// 705.484.450.52
-// 111.111.111.11
-
-
 class ValidaCPF{
     constructor(cpfEnviado){
         Object.defineProperty(this, 'cpfLimpo', {
             value: cpfEnviado.replace(/\D+/g, ''),
             writable: false,
-            enumerable: true,
+            enumerable: true ,
             configurable: false
         });
     }
 
     valida(){
-        if(!this.cpfLimpo) return false;
-        if(typeof this.cpfLimpo !== 'string') return false;
-        if(cpfLimpo.lenght !== 11) return false;
-        if(this.sequencia()) return false;
-
-    }
-
-    sequencia(){
-            return this.cpfLimpo[0].repeat(11) === this.cpfLimpo;
-    }
-
-    geraNovoCpf(){
-        const cpfSemDigitos = cpfLimpo.slice(0, -2);
-    }
-
-    geraDigitos(cpfSemDigitos){
-        let total = 0;
-        let regressivo = cpfSemDigitos.length + 1;
-        for(let valores of cpfSemDigitos){
-            total += regressivo * Number(valores);
-            regressivo--;
-        }
-
-        const digito = 11 - (total % 11);
-        return digito <= 9 ? String(digito) : '0';
+        if(!this.cpfLimpo) return false
+        if()
     }
 }
-
-const cpf = new ValidaCPF('070.987.720-03');
-console.log(cpf);
