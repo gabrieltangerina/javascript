0aa38bc04010a5a4fd3504d232d9253e14cdefec
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-01-31 00:03:26 -0300

    Aprendendo async/await

--- CursoUdemy/Async-Await/index.js
+++ CursoUdemy/Async-Await/index.js
@@ -1,24 +1,74 @@
-// function rand(min, max){
-//         // min *= 1000;
-//         // max *= 1000;
-//         return Math.floor(Math.random() * (max - min) + min);
-// }
+function rand(min = 0, max = 5){
+        // min *= 1000;
+        // max *= 1000;
+        return Math.floor(Math.random() * (max - min) + min);
+}
 
-function promise1(msg){
-    return new Promise((resolve) =>{
-        resolve(msg);
+function exibirMensagem(msg, tempo){
+    return new Promise((resolve, reject) =>{
+        setInterval(()=>{
+            if(typeof msg !== 'string') reject();
+            resolve(msg);
+        }, tempo)
     })
 }
 
-promise1('Aprendendo Promise')
-    .then((msg)=>{
-        console.log(msg);
-        return 'Encadeando Promises';
-    })
-    .then((msg)=>{
-        console.log(msg);
-        return 'Mais um encadeamento de Promise';
-    })
-    .then((msg)=>{
-        console.log(msg);
-    })
+// exibirMensagem('Fase 1', rand())
+//     .then( valor =>{
+//         console.log(valor);
+//         return exibirMensagem('Fase 2', rand());
+//     })
+//     .then(valor =>{
+//         console.log(valor);
+//         return exibirMensagem('Fase 3', rand());
+//     })
+//     .then(valor =>{
+//         console.log(valor);
+//         return exibirMensagem(`Terminamos na fase: ${valor}`);
+//     })
+//     .then(valor =>{
+//         console.log(valor);
+//     })
+//     .catch(e =>{
+//         console.log('ERRO.')
+//     })
+
+
+async function executa(){
+    try{
+        const fase1 = await exibirMensagem('Fase 1', rand());
+        console.log(fase1);
+    
+        const fase2 = await exibirMensagem('Fase 2', rand());
+        console.log(fase2);
+    
+        const fase3 = await exibirMensagem('Fase 3', rand());
+        console.log(fase3);
+    
+        console.log('Terminamos na fase: ', fase3);
+    } catch(e){
+        console.log('ERRO.', e);
+    }
+}
+
+executa();
+
+// Async vai permitir que você use a palavra await dentro de uma função para você esperar uma Promise ser finalizada
+
+
+
+
+
+// promise1('Aprendendo Promise')
+//     .then((msg)=>{
+//         console.log(msg);
+//         return 'Encadeando Promises';
+//     })
+//     .then((msg)=>{
+//         console.log(msg);
+//         return 'Mais um encadeamento de Promise';
+//     })
+//     .then((msg)=>{
+//         console.log(msg);
+//     })
+
