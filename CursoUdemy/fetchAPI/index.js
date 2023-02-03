4413ec6c0c411557ff831b9b2a3229b07074cba6
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-02-03 16:16:47 -0300

    Aprendendo mais sobre fetch

--- CursoUdemy/fetchAPI/index.js
+++ CursoUdemy/fetchAPI/index.js
@@ -1,44 +1,65 @@
-// document.addEventListener('click', (e) =>{
-//     e.preventDefault();
-//     const el = e.target;
-//     const atributo = el.getAttribute('href');
-//     const atributoDois = el.getAttribute('class');
-
-//     console.log(atributo, atributoDois);
-// })
-
-// const pegandoElemento = document.querySelector('.classePagina1');
-// const atributo = pegandoElemento.getAttribute('href');
-// console.log(atributo);
-
-
-document.addEventListener('click', e =>{
+document.addEventListener('click', (e) =>{
     const el = e.target;
     const tag = el.tagName.toLowerCase();
-
-    if (tag === 'a'){
+    
+    if(tag === 'a'){
         e.preventDefault();
-        carregaPagina(el);
+        carregaPagina(el); // aqui estava apenas 'e'
     }
 })
 
-async function carregaPagina(el){
+async function carregaPagina(el) {
+    
     try{
         const href = el.getAttribute('href');
-        const response = await fetch(href);
+        const dadoURL = await fetch(href); // Quando usar o fetch e não tiver o then, lembre-se de usar o await. Como nesse caso aqui. Lembre tambem qu eo fetch retorna uma Promise então é inevitável o uso de then ou async/await
 
-        if(response.status !== 200) throw new Error('ERRO 404!');
+        if(dadoURL.status !== 200) throw new Error('ERRO!.');
 
-        const html = await response.text();
+        const html = await dadoURL.text(); // E como nesse caso aqui também
         carregaResultado(html);
     } catch(e){
         console.log(e);
     }
 }
 
-function carregaResultado(response) {
+function carregaResultado(html){
     const resultado = document.querySelector('.resultado');
-    resultado.innerHTML = response;
+    resultado.innerHTML = html;
 }
 
-// getAttribute retorna o valor de um argumento específico do elemento. Pegar o valor de determinado atributo de um elemento.
+// LEMBRE-SE DE COLOCAR OS CUIDADOS PARA NÃO TER ERROS.
+
+
+//============================================================================
+// document.addEventListener('click', e =>{
+//     const el = e.target;
+//     const tag = el.tagName.toLowerCase();
+
+//     if(tag === 'a'){
+//         e.preventDefault();
+//         carregaPagina(el);
+//     }
+// })
+
+// async function carregaPagina(el){
+// try{
+//     const href = el.getAttribute('href'); // Pegando o link
+//     const response = await fetch(href); // Carrega o link
+
+//     if(response.status !== 200) throw new Error('ERRO 404!');
+
+//     const html = await response.text(); // Convertendo o dado do fetch em texto
+//     carregaResultado(html); // Chamando a função
+// } catch(e){
+//     console.log('Ocorreu um erro', e);
+// }
+// }
+
+// // Essa função pega a div com a classe resultado e adiciona nela o texto da URL que pegamos com o fetch.
+// function carregaResultado(response) {
+// const resultado = document.querySelector('.resultado');
+// resultado.innerHTML = response;
+// } 
+
+
